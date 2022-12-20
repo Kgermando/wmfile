d23583e1c560ac55b5bcaa10bb0365e9a62b1926
Germain Kataku <katakugermain@gmail.com>
2022-12-20 21:51:23 +0100

    change ip

--- src/uploadfile/uploadfile.controller.ts
+++ src/uploadfile/uploadfile.controller.ts
@@ -43,7 +43,7 @@ export class UploadfileController {
     }))
     uploadFile(@UploadedFile() file) {
         return {
-            url: `http://161.35.239.245:3000/uploadfile/${file.path}`
+            url: `http://192.168.100.200:3000/uploadfile/${file.path}`
         }
     }
 
